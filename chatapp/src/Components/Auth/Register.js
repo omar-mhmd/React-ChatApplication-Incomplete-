d10b617e25a091c7087b2dd9e02a7410d6f4b557
omar-mhmd <mouri700@gmail.com>
2020-02-05 19:43:03 +0200

    finished login and spinner page

--- chatapp/src/Components/Auth/Register.js
+++ chatapp/src/Components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase from "../../firebase";
+import md5 from "md5";
 import {
   Grid,
   Form,
@@ -16,47 +17,51 @@ class Register extends React.Component {
     username: "",
     email: "",
     password: "",
-    passwordconfirmation: "",
+    passwordConfirmation: "",
     errors: [],
-    loading: false
+    loading: false,
+    usersRef: firebase.database().ref("users")
   };
 
   isFormValid = () => {
-      let errors = []
-      let error;
-
+    let errors = [];
+    let error;
 
     if (this.isFormEmpty(this.state)) {
-     error = {message: 'please fill all fields'};
-     this.setState({errors: errors.concat(error)})
-     return false;
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
     } else if (!this.isPasswordValid(this.state)) {
-        error = {message: 'password is invalid'};
-        this.setState({errors: errors.concat(error)})
-        return false;
+      error = { message: "Password is invalid" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
     } else {
       return true;
     }
   };
 
-  isFormEmpty = ({username,email,password,passwordconfirmation}) => {
-     return !username.length || !email.length || !password.length || !passwordconfirmation.length
-  }
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
 
-  isPasswordValid = ({password,passwordconfirmation}) => {
-    if(password.length<6 || passwordconfirmation.length<6){
-        return false;
-    } 
-
-    else if (password !== passwordconfirmation){
-        return false;
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
     }
-    else {
-        return true;
-    }
-  }
+  };
 
-displayErrors = errors => errors.map((error,i) => <p key={i}>{error.message}</p>)
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
@@ -64,37 +69,71 @@ displayErrors = errors => errors.map((error,i) => <p key={i}>{error.message}</p>
   handleSubmit = event => {
     event.preventDefault();
     if (this.isFormValid()) {
-    this.setState({errors:[], loading:true})
+      this.setState({ errors: [], loading: true });
       firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
           console.log(createdUser);
-          this.setState({loading:false})
+          createdUser.user
+            .updateProfile({
+              displayName: this.state.username,
+              photoURL: `http://gravatar.com/avatar/${md5(
+                createdUser.user.email
+              )}?d=identicon`
+            })
+            .then(() => {
+              this.saveUser(createdUser).then(() => {
+                console.log("user saved");
+              });
+            })
+            .catch(err => {
+              console.error(err);
+              this.setState({
+                errors: this.state.errors.concat(err),
+                loading: false
+              });
+            });
         })
-
         .catch(err => {
-          console.log(err);
-          this.setState({errors:this.state.errors.concat(err), loading: false})
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false
+          });
         });
     }
   };
 
-  handleInputError = (errors,inputName) => {
-     return errors.some(error =>
-        error.message.toLowerCase().includes(inputName)
-        )
-        ? "error"
-        : ''
-  }
+  saveUser = createdUser => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL
+    });
+  };
+
+  handleInputError = (errors, inputName) => {
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? "error"
+      : "";
+  };
+
   render() {
-    const { username, email, password, passwordconfirmation,loading,errors } = this.state;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+      loading
+    } = this.state;
+
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="purple" textAlign="center">
-            <Icon name="american sign language interpreting" color="violet" />
-            Register for HolaChat!
+          <Header as="h1" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
@@ -108,6 +147,7 @@ displayErrors = errors => errors.map((error,i) => <p key={i}>{error.message}</p>
                 value={username}
                 type="text"
               />
+
               <Form.Input
                 fluid
                 name="email"
@@ -115,49 +155,59 @@ displayErrors = errors => errors.map((error,i) => <p key={i}>{error.message}</p>
                 iconPosition="left"
                 placeholder="Email Address"
                 onChange={this.handleChange}
-                className={this.handleInputError(errors,'email')}
                 value={email}
+                className={this.handleInputError(errors, "email")}
                 type="email"
               />
+
               <Form.Input
                 fluid
                 name="password"
                 icon="lock"
                 iconPosition="left"
                 placeholder="Password"
-                onChange={this.handleChange} 
-                className={this.handleInputError(errors,'password')}
+                onChange={this.handleChange}
                 value={password}
+                className={this.handleInputError(errors, "password")}
                 type="password"
               />
+
               <Form.Input
                 fluid
-                name="passwordconfirmation"
+                name="passwordConfirmation"
                 icon="repeat"
                 iconPosition="left"
                 placeholder="Password Confirmation"
                 onChange={this.handleChange}
-                className={this.handleInputError(errors,'passwordconfirmation')}
-                value={passwordconfirmation}
+                value={passwordConfirmation}
+                className={this.handleInputError(errors, "password")}
                 type="password"
               />
-              <Button disabled = {loading} className={loading ? 'loading' : ''}  color="violet" fluid size="large">
+
+              <Button
+                disabled={loading}
+                className={loading ? "loading" : ""}
+                color="orange"
+                fluid
+                size="large"
+              >
                 Submit
               </Button>
             </Segment>
           </Form>
-          {this.state.errors.length > 0 && (
-              <Message error>
-                  <h3>Error</h3>
-                  {this.displayErrors(this.state.errors)}
-              </Message>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
           )}
           <Message>
-            Already a user ? <Link to="/login"> Login </Link>
+            Already a user? <Link to="/login">Login</Link>
           </Message>
         </Grid.Column>
       </Grid>
     );
   }
 }
+
 export default Register;
--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-import Login from './Components/Auth/Login'
-import Register from './Components/Auth/Register'
-import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
+import Spinner from "./Spinner";
+import * as serviceWorker from "./serviceWorker";
+import "semantic-ui-css/semantic.min.css";
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import firebase from "./firebase";
 
-const Root = () => (
-    <Router>
+import "semantic-ui-css/semantic.min.css";
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    withRouter
+  } from "react-router-dom";
+  
+  import { createStore } from "redux";
+  import { Provider, connect } from "react-redux";
+  import { composeWithDevTools } from "redux-devtools-extension";
+  import rootReducer from "./reducers";
+  import { setUser } from "./actions";
+  
+  const store = createStore(rootReducer, composeWithDevTools());
+  
+  class Root extends React.Component {
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          // console.log(user);
+          this.props.setUser(user);
+          this.props.history.push("/");
+        }
+      });
+    }
+  
+    render() {
+      return this.props.isLoading ? (
+        <Spinner />
+      ) : (
         <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
-    </Router>
-)
+      );
+    }
+  }
+  
+  const mapStateFromProps = state => ({
+    isLoading: state.user.isLoading
+  });
+  
+  const RootWithAuth = withRouter(
+    connect(
+      mapStateFromProps,
+      { setUser }
+    )(Root)
+  );
+  
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <RootWithAuth />
+      </Router>
+    </Provider>,
+    document.getElementById("root")
+  );
+  
 
-
-
-ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
